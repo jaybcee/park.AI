a3b3500a9d56da97368e46f9aa62a34c09821c73
Nic MacBeth <nicolasmacbeth@gmail.com>
2019-01-20 10:26:05 -0500

    yo

---
+++ vars.js
@@ -0,0 +1,23 @@
+var mon = 'mongodb://main_user:Hackatown2019@ds159574.mlab.com:59574/hackatown';
+module.exports.mon = mon;
+
+var first = "ACc06048dbe0998b58b252e99eed4682a2";
+var second = "53fe941654e5e0c8bd69464356aa9d0a";
+module.exports.first = first;
+module.exports.second=second;
+
+var fromm = '+15146137260';
+module.exports.fromm = fromm;
+
+var imag = "https://hackatown2019.herokuapp.com/images/test";
+module.exports.imag = imag;
+
+var token = "3TN6kvVNjrKLxIojQ1dwTmRaS3kuWkmHUWEH";
+module.exports.token = token
+
+var one = "5146321797";
+var two = "5148823346";
+var three = "5147967305";
+module.exports.one = one;
+module.exports.two = two;
+module.exports.three = three;
